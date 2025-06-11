3d30e6c76136650c40b6fca1a70aaccf41cdb937
Termins55 <kevgame123@gmail.com>
2025-06-11 16:04:04 +0300

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+// 1) Створіть дві змінні a та b. Виведіть у консоль результат їх множення.
+
+const a = Number(10);
+const b = Number(50);
+const sum = a * b;
+console.log(sum);
+
+// 2) Створіть дві змінні c  та d. Виведіть у консоль результат ділення першого на друге.
+
+const c = Number(10);
+const d = Number(50);
+const sum1 = a / b;
+console.log(sum1);
+
+// 3) Створіть дві змінні e  та f. Виведіть у консоль результат додавання.
+
+const e = Number(10);
+const f = Number(50);
+const sum2 = a + b;
+console.log(sum2);
+
+// 4) Визначте змінні зі значеннями 11, true, "java script", "100"  і виведіть їх в консоль.
+
+console.log('typeof 11:', typeof 11);
+console.log('typeof true:', typeof true);
+console.log('typeof "java script":', typeof "java script");
+console.log('typeof "100":', typeof "100");
+
+// 5) *Переробіть наведений код так, щоб у ньому використовувалися операції +=, -=, *=, /=, ++, --
+// let num = 1;
+// num = num + 11;
+// num = num – 11;
+// num = num * 11;
+// num = num / 11;
+// num = num + 1;
+// num = num – 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
